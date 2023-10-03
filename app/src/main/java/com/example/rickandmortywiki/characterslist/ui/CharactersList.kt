cb6892344691eb7a3a299fe5c28437b060eeaea4
Emanuelle M <emanuelle.menalii@gmail.com>
2023-10-03 20:57:11 -0300

    add margins

--- app/src/main/java/com/example/rickandmortywiki/characterslist/ui/CharactersList.kt
+++ app/src/main/java/com/example/rickandmortywiki/characterslist/ui/CharactersList.kt
@@ -2,12 +2,14 @@ package com.example.rickandmortywiki.characterslist.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -35,6 +37,7 @@ fun CharacterGrid(
     onDetailClick: (id: String) -> Unit,
 ) {
     LazyVerticalGrid(
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp),
         columns = GridCells.Adaptive(minSize = 128.dp),
     ) {
         items(characters.size) { item ->
@@ -50,6 +53,7 @@ fun CharacterItem(
     onDetailClick: (id: String) -> Unit,
 ) {
     Card(
+        modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp),
         onClick = {
             character.id?.let { id ->
                 onDetailClick(id)
