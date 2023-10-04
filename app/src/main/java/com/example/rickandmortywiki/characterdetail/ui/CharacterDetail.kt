21549c489f25b11904114cabf19720a1044bc3a5
Emanuelle M <emanuelle.menalii@gmail.com>
2023-10-03 21:08:13 -0300

    add margins

--- app/src/main/java/com/example/rickandmortywiki/characterdetail/ui/CharacterDetail.kt
+++ app/src/main/java/com/example/rickandmortywiki/characterdetail/ui/CharacterDetail.kt
@@ -1,9 +1,15 @@
 package com.example.rickandmortywiki.characterdetail.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.rickandmortywiki.characterdetail.viewmodel.CharacterDetailViewModel
 import com.example.rickandmortywiki.characterslist.ui.AppBarTopBack
@@ -16,9 +22,17 @@ fun CharacterDetail(
 ) {
     characterDetailViewModel.queryCharacterDetail(characterId)
     val character = characterDetailViewModel.charactersDetail.collectAsState().value.characters
-    Column {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         AppBarTopBack("", backToHome)
-        AsyncImage(model = character.image, contentDescription = null)
-        Text(text = character.name ?: "")
+        AsyncImage(
+            modifier = Modifier
+                .width(300.dp)
+                .height(300.dp),
+            model = character.image,
+            contentDescription = null,
+        )
+        Text(modifier = Modifier.padding(vertical = 32.dp), text = character.name ?: "")
     }
 }
