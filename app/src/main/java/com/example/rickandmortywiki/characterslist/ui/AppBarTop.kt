7dadc3b128cbd70fb8bbef359d6ef4d68a5fa718
Emanuelle M <emanuelle.menalii@gmail.com>
2025-04-03 08:41:50 -0300

    -ugrade gradle version -adjustments on button click behavior

--- app/src/main/java/com/example/rickandmortywiki/characterslist/ui/AppBarTop.kt
+++ app/src/main/java/com/example/rickandmortywiki/characterslist/ui/AppBarTop.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -39,5 +40,15 @@ fun AppBarTopBack(
                 Icon(Icons.Filled.ArrowBack, "pesquisa personagem")
             }
         },
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true)
+@Composable
+fun TopAppBarPreview(){
+    TopAppBar(
+        title = {Text("Rick and morty api")},
+        navigationIcon = {}
     )
 }
--- app/src/main/java/com/example/rickandmortywiki/characterslist/ui/SearchCharacter.kt
+++ app/src/main/java/com/example/rickandmortywiki/characterslist/ui/SearchCharacter.kt
@@ -6,17 +6,21 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
+import com.example.rickandmortywiki.ui.theme.Purple40
+import com.example.rickandmortywiki.ui.theme.PurpleGrey40
 
 @Composable
 fun SearchCharacter(
@@ -29,31 +33,54 @@ fun SearchCharacter(
         AppBarTopBack("Filtro", backToHome)
         Column {
             var characterName = ""
-            var selectedState = "alive"
+            var selectedState by remember { mutableStateOf(characterName) }
 
             Text(text = "Nome")
 
             SimpleFilledTextFieldSample { characterName = it }
 
-            Column(modifier = Modifier.padding(vertical = 8.dp)) {
+            Column(
+                modifier = Modifier
+                    .padding(vertical = 8.dp)
+            ) {
                 Text(text = "Status")
 
                 Row {
-                    Button(modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp),
+                    Button(
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp, vertical = 8.dp),
                         onClick = {
                             selectedState = "alive"
-                        }) {
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = if (selectedState == "alive") Purple40 else PurpleGrey40),
+                    )
+                    {
                         Text(text = "alive")
                     }
-                    Button(modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp),
-                        onClick = { selectedState = "dead" }) {
+                    Button(
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp, vertical = 8.dp),
+                        onClick = {
+                            selectedState = "dead"
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = if (selectedState == "dead") Purple40 else PurpleGrey40),
+
+                        ) {
                         Text(text = "dead")
                     }
-                    Button(modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp),
-                        onClick = { selectedState = "unknow" }) {
+                    Button(
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp, vertical = 8.dp),
+                        onClick = {
+                            selectedState = "unknow"
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = if (selectedState == "unknow") Purple40 else PurpleGrey40),
+                    ) {
+
                         Text(text = "unknow")
                     }
                 }
+
                 Spacer(modifier = Modifier.padding(40.dp))
                 Button(
                     modifier = Modifier
